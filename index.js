c357bf414ce7b671207e89361f42bf8c1c5abaaa
Ashis Kumar Joy <ashis263@gmail.com>
2025-02-22 08:42:19 +0600

    added remote origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { createServer } = require("http");
 const app = express();
 const port = process.env.PORT || 5000;
 const server = createServer(app);
-const io = new Server(server, { cors: { origin: "http://localhost:5173" } });
+const io = new Server(server, { cors: { origin:[ "http://localhost:5173", "https://task-flow-c8b5e.web.app/"] } });
 
 //middlewares
 app.use(cors());
