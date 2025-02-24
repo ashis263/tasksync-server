047eced7fa1813caa1ce1908f516145e08c70978
Ashis Kumar Joy <ashis263@gmail.com>
2025-02-24 21:56:01 +0600

    updated task getting and adding with express api instaed of socket io

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [ "http://localhost:5173", "https://task-flow-c8b5e.web.app"],
+    origin: ["http://localhost:5173", "https://task-flow-c8b5e.web.app"],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
   }
@@ -42,6 +42,19 @@ async function run() {
     const taskCollection = client.db('task-sync').collection('tasks');
     const activityCollection = client.db('task-sync').collection('activities');
 
+    io.on("connection", (socket) => {
+      console.log("User connected:", socket.id);
+      socket.on('disconnect', () => {
+        console.log('User disconnected');
+      });
+    });
+
+    const sendTasks = async (email) => {
+      result = await taskCollection.find({ addedBy: email }).sort({ _id: -1 }).toArray();
+      io.emit('tasks', result);
+      console.log(result);
+    }
+
     //user related api
     app.put('/users', async (req, res) => {
       const query = { email: req.body.email };
@@ -53,70 +66,71 @@ async function run() {
       res.send(result);
     });
 
-    io.on("connection", (socket) => {
-      console.log("User connected:", socket.id);
-
-      socket.on('addTask', async (task) => {
-        result = await taskCollection.insertOne(task);
-        if (result.insertedId) {
-          socket.emit('taskAdded', 'Task Added')
-        }
-      });
-
-      socket.on('getTasks', async (email) => {
-        result = await taskCollection.find({ addedBy: email }).sort({ _id: -1 }).toArray();
-        socket.emit('tasks', result);
-      })
-
-      socket.on('deleteTask', async (id) => {
-        const query = { _id: new ObjectId(id) }
-        const result = await taskCollection.deleteOne(query);
-        if (result.deletedCount) {
-          socket.emit('taskDeleted');
-        }
-      })
-
-      socket.on('modified', async (data) => {
-        const result = await activityCollection.insertOne(data);
-      })
-
-      socket.on("movedCategory", async (data) => {
-        const updatedDoc = {
-          $set: {
-            category: data.to
-          }
-        }
-        const result = await taskCollection.updateOne({ _id: new ObjectId(data.id) }, updatedDoc);
-        if (result.modifiedCount) {
-          socket.emit('categoryModified');
-        }
-      })
-
-      socket.on('update', async (data) => {
-        console.log(data);
-        const { oldId, ...other } = data;
-        const updatedDoc = {
-          $set: {
-            title: other.title,
-            description: other.description,
-            category: other.category,
-            deadline: other.deadline
-          }
-        }
-        const result = await taskCollection.updateOne({ _id: new ObjectId(oldId) }, updatedDoc);
-        if (result.modifiedCount) {
-          socket.emit('updated');
-        }
-      })
-
-      socket.on('getActivities', async (email) => {
-        const result = await activityCollection.find({ user: email }).sort({ _id: -1 }).toArray();
-        socket.emit('activities', result);
-      })
-      socket.on('disconnect', () => {
-        console.log('User disconnected');
-      });
+    //task related
+    app.get('/tasks', async (req, res) => {
+      result = await taskCollection.find({ addedBy: req.query.email }).sort({ _id: -1 }).toArray();
+      res.send(result);
     })
+
+    app.post('/tasks', async (req, res) => {
+      result = await taskCollection.insertOne(req.body);
+      sendTasks(req.body.addedBy);
+    });
+
+    //   socket.on('getTasks', async (email) => {
+    //     result = await taskCollection.find({ addedBy: email }).sort({ _id: -1 }).toArray();
+    //     socket.emit('tasks', result);
+    //   })
+
+    //   socket.on('deleteTask', async (id) => {
+    //     const query = { _id: new ObjectId(id) }
+    //     const result = await taskCollection.deleteOne(query);
+    //     if (result.deletedCount) {
+    //       socket.emit('taskDeleted');
+    //     }
+    //   })
+
+    //   socket.on('modified', async (data) => {
+    //     const result = await activityCollection.insertOne(data);
+    //   })
+
+    //   socket.on("movedCategory", async (data) => {
+    //     const updatedDoc = {
+    //       $set: {
+    //         category: data.to
+    //       }
+    //     }
+    //     const result = await taskCollection.updateOne({ _id: new ObjectId(data.id) }, updatedDoc);
+    //     if (result.modifiedCount) {
+    //       socket.emit('categoryModified');
+    //     }
+    //   })
+
+    //   socket.on('update', async (data) => {
+    //     console.log(data);
+    //     const { oldId, ...other } = data;
+    //     const updatedDoc = {
+    //       $set: {
+    //         title: other.title,
+    //         description: other.description,
+    //         category: other.category,
+    //         deadline: other.deadline
+    //       }
+    //     }
+    //     const result = await taskCollection.updateOne({ _id: new ObjectId(oldId) }, updatedDoc);
+    //     if (result.modifiedCount) {
+    //       socket.emit('updated');
+    //     }
+    //   })
+
+    //   socket.on('getActivities', async (email) => {
+    //     const result = await activityCollection.find({ user: email }).sort({ _id: -1 }).toArray();
+    //     socket.emit('activities', result);
+    //   })
+    //   socket.on('disconnect', () => {
+    //     console.log('User disconnected');
+    //   });
+    // })
 
 
     // Send a ping to confirm a successful connection
